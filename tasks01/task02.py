8a8cb89785a82ee94bf93bd22a6b6f3630ab062b
FJHleo <fjhleo@gmail.com>
2024-02-20 13:15:21 +0300

    I create a new folder for first tasks

---
+++ tasks01/task02.py
@@ -0,0 +1,27 @@
+from math import exp
+
+def f(x):
+    return exp(x)
+
+def CenterRecthangles(left,right,steps):
+    result=0
+    step=(right-left)/steps
+    i=0
+    for _ in range (steps):
+        result +=f(step/2+step*i)*step
+        i+=1
+    return result
+ 
+def PrintSolution():
+    left=0
+    right=1
+    N=10
+    trueIntegral =exp(1)-1
+    for _ in range (9):
+        answer = CenterRecthangles(left,right,N)
+        print('N=',N,'\tError=',trueIntegral -answer)
+        N *=10
+    
+    
+    
+PrintSolution()
